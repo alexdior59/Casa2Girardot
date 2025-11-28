4b99e0145018d0f7c6a1e805b1a357b3fe9ac013
Julian Diaz <alexdior59@gmail.com>
2025-11-28 12:52:27 -0500

    Actualizar dependencias en `pom.xml` (downgrade Spring Boot a 3.3.5, upgrade Springdoc a 2.6.0); ajustar configuración de seguridad.

--- src/main/java/org/example/casa2girardot/Security/SecurityConfig.java
+++ src/main/java/org/example/casa2girardot/Security/SecurityConfig.java
@@ -27,12 +27,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
+
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
